663052a75a72c41bd6333766482d10de36ecce75
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2013-12-11 23:21:00 +0000

    Corrected setup.py paths for UNIX builds.

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
 	name = 'Flipper',
 	version = Flipper_version,  # Get the version from the Options class.
 	description = 'For manipulating curves on surfaces and producing mapping tori.',
-	options = {'build_exe':{'icon':'.\\Source\\Icon\\Icon.ico', 'include_files':['.\\Source\\Icon']}},
+	options = {'build_exe':{'icon':'./Source/Icon/Icon.ico', 'include_files':['./Source/Icon']}},
 	executables = [Executable('Flipper.py', base=('Win32GUI' if sys.platform == 'win32' else None))]
 	)

ea209a6b8ee3d73e7587f25bdf798d073af5097a
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-03-03 23:04:36 +0000

    Added missing files including polynomial.py.

---
+++ tests/polynomial.py
@@ -0,0 +1,19 @@
+
+import Flipper
+
+def main():
+	f = Flipper.Polynomial([-2, 0, 1])
+	AA = f.algebraic_approximate_leading_root(10)
+	if (AA * AA) != 2:
+		print(f, AA)
+		return False
+	
+	BB = f.algebraic_approximate_leading_root(100, power=2)
+	if BB != 2:
+		print(f, BB)
+		return False
+	
+	return True
+
+if __name__ == '__main__':
+	print(main())

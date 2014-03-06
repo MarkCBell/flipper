b1099fa5b8060481793dcb9b972edff0f35237b4
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-03-06 17:28:39 +0000

    More PyLint minor changes.

--- kernel/error.py
+++ kernel/error.py
@@ -2,6 +2,7 @@
 # An exception for aborting computations with. This is thrown by clicking 'cancel' on a progress box.
 class AbortError(Exception):
 	def __init__(self, code=None):
+		super(AbortError, self).__init__()
 		self.code = code
 	def __str__(self):
 		return repr(self.code)
@@ -9,6 +10,7 @@ class AbortError(Exception):
 # An exception for when computations fail.
 class ComputationError(Exception):
 	def __init__(self, code=None):
+		super(ComputationError, self).__init__()
 		self.code = code
 	def __str__(self):
 		return repr(self.code)
@@ -16,12 +18,14 @@ class ComputationError(Exception):
 # An exception for when a function notices that one of its assumptions is false.
 class AssumptionError(Exception):
 	def __init__(self, code=None):
+		super(AssumptionError, self).__init__()
 		self.code = code
 	def __str__(self):
 		return repr(self.code)
 
 class ApproximationError(Exception):
 	def __init__(self, code=None):
+		super(ApproximationError, self).__init__()
 		self.code = code
 	def __str__(self):
 		return repr(self.code)

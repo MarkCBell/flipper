586d6476e6d5fb599a86090cc1c575ab600c8221
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-03-14 11:18:19 +0000

    Added more tests.

--- tests/permutation.py
+++ tests/permutation.py
@@ -1,17 +1,20 @@
 
 from __future__ import print_function
-from itertools import permutations
+from itertools import product, permutations
 
 import Flipper
 
 def main():
-	
-	# !?! TO DO.
-	
-	all_perms = [Flipper.kernel.Permutation(perm) for perm in permutations(range(4), 4)]
+	# Get some example permutations.
+	all_perms = Flipper.kernel.permutation.all_permutations(4)
 	
 	try:
+		# Check that there are the same number of odd and even permutations.
 		assert(len([perm for perm in all_perms if perm.is_even()]) == len([perm for perm in all_perms if not perm.is_even()]))
+		# Check that composition respects parity.
+		assert(all((p1 * p2).is_even() == p1.is_even() ^ p2.is_even() ^ True for p1, p2 in product(all_perms, all_perms)))
+		# Check that every permutation acts transitively.
+		assert(all(set(perm * p1 for p1 in all_perms) == set(all_perms) for perm in all_perms))
 	except AssertionError:
 		return False
 	

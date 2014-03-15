85abe59faf798a257808a409c621d7435fc92308
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-03-15 00:15:50 +0000

    Readded iterative convergence methods to Polynomial.

--- kernel/polynomial.py
+++ kernel/polynomial.py
@@ -14,15 +14,84 @@ class Polynomial(object):
 		self.degree = len(self.coefficients) - 1
 		self.algebraic_approximation = self.degree * self.height
 		self.accuracy = 0
+		r = max(self.degree, 1) * self.height
+		self.interval = Flipper.kernel.Interval(-r, r, 0)
 	
+	def copy(self):
+		return Polynomial(self.coefficients)
+
+	def __eq__(self, other):
+		return self.coefficients == other.coefficients
+	def __ne__(self, other):
+		return self.coefficients != other.coefficients
+	def __neg__(self):
+		return Polynomial([-x for x in self])
+
 	def __iter__(self):
 		return iter(self.coefficients)
-	
+
 	def __getitem__(self, item):
 		return self.coefficients[item]
 	
+	def __call__(self, other):
+		return sum(a * other**index for index, a in enumerate(self))
+	def __mod__(self, other):
+		if isinstance(other, Polynomial):
+			if other.degree < 0: 
+				raise ZeroDivisionError
+			
+			N = self.copy()
+			D = other.copy()
+			while N.degree >= D.degree:
+				# N = D[-1] * N - N[-1] * D.shift(N.degree - D.degree)
+				N = Polynomial([D[-1] * coeffN - coeffd * N[-1] for coeffN, coeffd in zip(N, [0]*(N.degree - D.degree) + D.coefficients)])
+			return N
+		else:
+			return NotImplemented
+	
+	def _call_fraction(self, numerator, denominator):
+		return sum(coefficient * numerator**index * denominator**(self.degree - index) for index, coefficient in enumerate(self)), denominator**self.degree
+	
+	def signs_at_interval_endpoints(self, interval):
+		s1 = sum(coefficient * interval.lower**index * 10**(interval.precision*(self.degree - index)) for index, coefficient in enumerate(self))
+		s2 = sum(coefficient * interval.upper**index * 10**(interval.precision*(self.degree - index)) for index, coefficient in enumerate(self))
+		return (-1 if s1 < 0 else 0 if s1 == 0 else +1), (-1 if s2 < 0 else 0 if s2 == 0 else +1)
+	
+	def sturm_chain(self):
+		f1 = self
+		f2 = self.derivative()
+		sturm_chain = [f1, f2]
+		while sturm_chain[-1] != Polynomial([]):
+			sturm_chain.append(-(sturm_chain[-2] % sturm_chain[-1]))
+		
+		return sturm_chain
+	
+	def num_roots(self, interval):
+		lower_signs, upper_signs = zip(*[f.signs_at_interval_endpoints(interval) for f in self.sturm_chain()])
+		lower_non_zero_signs = [x for x in lower_signs if x != 0]
+		upper_non_zero_signs = [x for x in upper_signs if x != 0]
+		lower_sign_changes = sum(1 if x * y < 0 else 0 for x, y in zip(lower_non_zero_signs, lower_non_zero_signs[1:]))
+		upper_sign_changes = sum(1 if x * y < 0 else 0 for x, y in zip(upper_non_zero_signs, upper_non_zero_signs[1:]))
+		return lower_sign_changes - upper_sign_changes
+
+	def subdivide_iterate(self, interval):
+		return [I for I in interval.subdivide() if self.num_roots(I) > 0][-1]
+	
 	def is_monic(self):
 		return abs(self[-1]) == 1
+	
+	def derivative(self):
+		return Polynomial([a * index for index, a in enumerate(self)][1:])
+	
+	def NR_iterate(self, interval):
+		# For why this works see: http://www.rz.uni-karlsruhe.de/~iam/html/language/cxsc/node12.html
+		m = (interval.lower + interval.upper) // 2
+		k = 10
+		m2 = m * 10**k
+		J = Flipper.kernel.Interval(m2 - 1, m2 + 1, interval.precision + k)
+		K = J - self(J) / self.derivative()(interval)
+		L = K.change_denominator(K.accuracy * 2)
+		return L.intersect(interval)
 	
 	def increase_accuracy(self, accuracy):
 		# Eventually we will find the interval ourselves, however at the minute sage is much faster so

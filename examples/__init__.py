1c945a19c20cffee608f82fac6367e611a54994b
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-09-06 20:39:51 +0100

    Corrected modules imported.

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,16 +1,5 @@
 
 from . import abstracttriangulation
-from . import encoding
-from . import invariant_lamination
-from . import lamination
-from . import layeredtriangulation
-from . import matrix
-from . import splitting_sequence
-
-from . import degeneracy_slopes
-from . import distribution
-from . import hard_invariant_lamination
-from . import knot_cusp_orders
 
 ExampleSurface = abstracttriangulation.ExampleSurface
 

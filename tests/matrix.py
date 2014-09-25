e98fe614030b0cc930f608c1617f3a147aa7f332
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-09-25 18:38:33 +0100

    Added Matrix tests.

--- tests/matrix.py
+++ tests/matrix.py
@@ -7,8 +7,16 @@ def main(verbose=False):
 	if verbose: print('Running matrix tests.')
 	
 	# !?! Add test here.
+	M = flipper.kernel.Matrix([[2, 1], [1, 1]])
+	N = flipper.kernel.Matrix([[1, -1], [-1, 2]])
 	
-	return True
+	tests = [
+		M * N == flipper.kernel.id_matrix(2),
+		M.char_poly() == flipper.kernel.Polynomial([1, -3, 1]),
+		M.determinant() == 1
+		]
+	
+	return all(tests)
 
 if __name__ == '__main__':
 	print(main(verbose=True))

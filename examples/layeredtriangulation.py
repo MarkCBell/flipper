b949b95b9dba12f2407c73c3f4267548e1a40d3a
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-09-29 08:50:49 +0100

    Better examples.

--- examples/layeredtriangulation.py
+++ examples/layeredtriangulation.py
@@ -10,15 +10,15 @@ def main():
 	# This process will fail (with an AssumptionError or ComputationError) if our map is not pseudo-Anosov.
 	S = flipper.load.equipped_triangulation('S_1_2')
 	h = S.mapping_class(word)
+	print('Built the mapping class h := \'%s\'.' % word)
 	splittings = h.splitting_sequences()  # A list of splitting sequences.
 	# There may be more than one isometry, for now let's just pick the first. We'll worry about this eventually.
 	bundle = splittings[0].bundle()
-	print('Built the bundle with monodromy h=\'%s\'.' % word)
+	print('Built the bundle with monodromy h.')
 	print('It is the first of %d sisters.' % len(splittings))
 	print('It has %d cusp(s) with the following properties:' % bundle.num_cusps)
 	for index, (real, fibre, degeneracy) in enumerate(zip(bundle.real_cusps, bundle.fibre_slopes, bundle.degeneracy_slopes)):
 		print('\tCusp %s (%s): Fibre slope %s, degeneracy slope %s' % (index, 'Real' if real else 'Fake', fibre, degeneracy))
-	print('To build this bundle I may have had to create some fake cusps.')
 	
 	M = snappy.Manifold(bundle.snappy_string())
 	print('The manifold M: %s' % M)

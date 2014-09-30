c46fc162a0d2fcefe9c24c8472e64102e51a457f
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-09-30 15:57:25 +0100

    Updated description.

--- freeze.py
+++ freeze.py
@@ -9,7 +9,7 @@ setup(
 	name='flipper',
 	version=str(version),  # Get the correct version.
 	author='Mark Bell',
-	description='For manipulating curves and measured laminatins on surfaces and producing mapping tori.',
+	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
 	options={'build_exe': {'icon': './application/icon/icon.ico', 'include_files': [('./application/icon/', './icon'), ('./application/docs/', './docs')]}},
 	executables=[Executable('app.py', base=('Win32GUI' if sys.platform == 'win32' else None))]
 	)

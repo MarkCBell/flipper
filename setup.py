b6dc554c2297faac046d913ca7f291b5c497fe51
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-09-30 17:32:20 +0100

    Modified setup.py to build from anywhere. flipper is now on PyPI under the name flipper-topology!

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 
 from distutils.core import setup, Command
+import os
 
 # Get the correct version.
 from versions import version
@@ -39,7 +40,7 @@ class ProfileCommand(Command):
 		flipper.profile.main()
 
 setup(
-	name='flipper',
+	name='flipper-topology',  # The name 'flipper' is taken on PyPI.
 	version=str(version),
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
 	author='Mark Bell',
@@ -47,7 +48,7 @@ setup(
 	url='https://bitbucket.org/Mark_Bell/flipper',
 	# Remember to update these if the directory structure changes.
 	packages=['flipper', 'flipper.application', 'flipper.load', 'flipper.examples', 'flipper.kernel', 'flipper.tests', 'flipper.profiles'],
-	package_dir={'flipper': ''},
+	package_dir={'flipper': os.path.dirname(__file__)},
 	package_data={
 		'flipper': ['./README', './LICENSE', 'docs/*'],
 		'flipper.application': ['icon/*', 'docs/*'],

7cae0e476f28cdba4e14b281f9529f64a9927b33
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-09-30 11:54:44 +0100

    EquippedTriangulations now decompose words. Examples no longer automatically loaded - some require snappy.

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,14 +1,3 @@
 
 ''' Some examples of how to use the flipper kernel. '''
 
-from . import canonical_sizes
-from . import degeneracy_slopes
-from . import distribution
-from . import encoding
-from . import hard_invariant_lamination
-from . import invariant_lamination
-from . import knot_cusp_orders
-from . import layeredtriangulation
-from . import non_geometric_bundles
-from . import splitting_sequence
-

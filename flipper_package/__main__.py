0ed8b903ba16752fe18a14bfeaa328bdcd6613ec
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-10-01 15:19:19 +0100

    Restructured package to make it work with setuptools.

--- flipper_package/__main__.py
+++ flipper_package/__main__.py
@@ -2,11 +2,12 @@
 import flipper
 
 def main():
-	print('flipper %s' % flipper.version)
+	print('flipper %s' % flipper.__version__)
 	print('To start the flipper GUI use:')
 	print('  > python -m flipper.app')
 	print('To test your installation of flipper use:')
 	print('  > python -m flipper.test')
+	print('Or start Python and import flipper directly.')
 
 if __name__ == '__main__':
 	main()
--- freeze.py
+++ freeze.py
@@ -3,11 +3,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Get the correct version.
-from versions import version
+from flipper.version import __version__
 
 setup(
 	name='flipper',
-	version=str(version),  # Get the correct version.
+	version=__version__,  # Get the correct version.
 	author='Mark Bell',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
 	options={'build_exe': {'icon': './application/icon/icon.ico', 'include_files': [('./application/icon/', './icon'), ('./application/docs/', './docs')]}},

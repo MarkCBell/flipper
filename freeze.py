4796a54df47a23e5ceb8ee63cd725608ad31eb8d
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-10-01 15:54:24 +0100

    Moved documentation into docs folder. Switched package name to flipper-triangulation.

--- freeze.py
+++ freeze.py
@@ -1,8 +1,10 @@
 
 import sys
+import os
 from cx_Freeze import setup, Executable
 
 # Get the correct version.
+flipper_directory = os.path.join(os.path.dirname(__file__), 'flipper_package')
 from flipper.version import __version__
 
 setup(
@@ -10,7 +12,8 @@ setup(
 	version=__version__,  # Get the correct version.
 	author='Mark Bell',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
-	options={'build_exe': {'icon': './application/icon/icon.ico', 'include_files': [('./application/icon/', './icon'), ('./application/docs/', './docs')]}},
-	executables=[Executable('app.py', base=('Win32GUI' if sys.platform == 'win32' else None))]
+	package_dir={'flipper': flipper_directory},
+	options={'build_exe': {'icon': './flipper_package/application/icon/icon.ico', 'include_files': [('./flipper_package/application/icon/', './icon'), ('./flipper_package/application/docs/', './flipper_package/docs')]}},
+	executables=[Executable('./flipper_package/app.py', base=('Win32GUI' if sys.platform == 'win32' else None))]
 	)
 

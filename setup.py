18198867efd464e46d57548d4270031c834786e4
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-10-01 14:07:44 +0100

    setup.py simplified. Updated README. About to try restructuring package for setuptools.

--- setup.py
+++ setup.py
@@ -1,43 +1,9 @@
 
-from __future__ import print_function
-
 from distutils.core import setup, Command
+#from setuptools import setup, Command
 import os
 
-# Get the correct version.
-from versions import version
-
-# So we can access all of the test suite just by doing 'python setup.py test'
-class TestCommand(Command):
-	description = 'Runs all tests in the tests directory.'
-	user_options = []
-	
-	def initialize_options(self):
-		pass
-	
-	def finalize_options(self):
-		pass
-	
-	def run(self):
-		import flipper
-		
-		flipper.test.main()
-
-# So we can access all of the profiling suite just by doing 'python setup.py profile'
-class ProfileCommand(Command):
-	description = 'Runs all tests in the tests directory.'
-	user_options = []
-	
-	def initialize_options(self):
-		pass
-	
-	def finalize_options(self):
-		pass
-	
-	def run(self):
-		import flipper
-		
-		flipper.profile.main()
+from versions import version  # Get the correct version number.
 
 setup(
 	name='flipper-topology',  # The name 'flipper' is taken on PyPI.
@@ -50,10 +16,9 @@ setup(
 	packages=['flipper', 'flipper.application', 'flipper.load', 'flipper.examples', 'flipper.kernel', 'flipper.tests', 'flipper.profiles'],
 	package_dir={'flipper': os.path.dirname(__file__)},
 	package_data={
-		'flipper': ['./README', './LICENSE', 'docs/*'],
+		'': ['./README', './LICENSE', 'docs/*'],
 		'flipper.application': ['icon/*', 'docs/*'],
 		'flipper.load': ['censuses/*.dat']
-		},
-	cmdclass={'test': TestCommand, 'profile': ProfileCommand}
+		}
 	)
 

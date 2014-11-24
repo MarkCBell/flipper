343da5f8cdd11a12fe4df18c074eb849d36a8022
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2014-11-24 13:37:14 +0000

    Added arrows to app. Rewriting progressbar.

--- flipper_triangulation/app/options.py
+++ flipper_triangulation/app/options.py
@@ -29,18 +29,22 @@ class Options(object):
 		
 		self.render_lamination_var = TK.StringVar(value=RENDER_LAMINATION_FULL)
 		self.show_internals_var = TK.BooleanVar(value=False)
+		self.show_orientations_var = TK.BooleanVar(value=False)
 		self.label_edges_var = TK.StringVar(value=LABEL_EDGES_NONE)
 		self.size_var = TK.IntVar(value=SIZE_SMALL)
 		
 		self.render_lamination = RENDER_LAMINATION_FULL
 		self.show_internals = False
+		self.show_orientations = False
 		self.label_edges = LABEL_EDGES_NONE
 		self.line_size = 2
 		self.dot_size = 3
+		self.arrow_shape = (12, 15, 5)
 		
 		# Set it so that self.update() will be called whenever these variables are changed.
 		self.render_lamination_var.trace('w', self.update)
 		self.show_internals_var.trace('w', self.update)
+		self.show_orientations_var.trace('w', self.update)
 		self.label_edges_var.trace('w', self.update)
 		self.size_var.trace('w', self.update)
 		
@@ -54,20 +58,24 @@ class Options(object):
 	def update(self, *args):
 		self.render_lamination = str(self.render_lamination_var.get())
 		self.show_internals = bool(self.show_internals_var.get())
+		self.show_orientations = bool(self.show_orientations_var.get())
 		self.label_edges = str(self.label_edges_var.get())
 		if self.size_var.get() == SIZE_SMALL:
 			self.line_size = 2
 			self.dot_size = 3
+			self.arrow_shape = (12, 15, 5)
 			self.application_font.configure(size=10)
 			self.canvas_font.configure(size=10)
 		elif self.size_var.get() == SIZE_MEDIUM:
 			self.line_size = 4
 			self.dot_size = 5
+			self.arrow_shape = (16, 20, 6)
 			self.application_font.configure(size=11)
 			self.canvas_font.configure(size=12)
 		elif self.size_var.get() == SIZE_LARGE:
 			self.line_size = 6
 			self.dot_size = 7
+			self.arrow_shape = (24, 30, 9)
 			self.application_font.configure(size=12)
 			self.canvas_font.configure(size=14)
 		

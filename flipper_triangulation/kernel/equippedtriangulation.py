59ec67238b3d3649bd6a5b1f8750317c85619d4b
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2015-01-06 19:35:38 +0000

    Initial version of EquippedTriangulation.all_words().

--- flipper_triangulation/kernel/equippedtriangulation.py
+++ flipper_triangulation/kernel/equippedtriangulation.py
@@ -76,6 +76,39 @@ class EquippedTriangulation(object):
 		
 		return '.'.join(choice(available_letters) for _ in range(length))
 	
+	def all_words(self, length, reduced=True, conjugate=True, inverse=True, prefix=None, _first=True):
+		''' Yield all words of given length.
+		
+		If reduced is set then obviously freely reducible words are skipped.
+		If conjugate is set then obviously cyclically reducible words are skipped.
+		If inverse is set then words that can be made earlier by inverting are skipped.
+		Inverse implies conjugate. '''
+		
+		if prefix is None: prefix = []
+		if inverse: conjugate = True
+		
+		if _first:
+			for word in self.all_words(length, reduced, conjugate, inverse, prefix, False):
+				yield '.'.join(word)
+		else:
+			if length > 0:
+				for letter in self.mapping_classes:
+					if not reduced or not prefix or letter != prefix[-1].swapcase():
+						prefix2 = prefix + [letter]
+						prefix2_inv = [x.swapcase() for x in prefix2[::-1]]
+						lp = len(prefix2)
+						if not conjugate or all(prefix2[i:2*i] >= prefix2[:min(i, len(prefix2)-i)] for i in range(lp // 2, lp)):
+							for word in self.all_words(length-1, reduced, conjugate, inverse, prefix2, False):
+								yield word
+			else:
+				prefix_inv = [x.swapcase() for x in prefix[::-1]]
+				if not conjugate or not prefix or prefix[0] != prefix[-1].swapcase():
+					if not conjugate or all(prefix[i:] + prefix[:i] >= prefix for i in range(len(prefix))):
+						if not inverse or all(prefix_inv[i:] + prefix_inv[:i] >= prefix for i in range(len(prefix))):
+							yield prefix
+		
+		return
+	
 	def decompose_word(self, word):
 		''' Return a list of mapping_classes keys whose concatenation is word and the keys are chosen greedly.
 		

48c76ecfb2c5439a398f9e417726a6229c434200
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2015-05-11 15:47:58 +0100

    Better lll test structure.

--- source/test/lll.py
+++ source/test/lll.py
@@ -3,68 +3,41 @@ import flipper
 def main(verbose=False):
 	if verbose: print('Running LLL tests.')
 	
-	M = flipper.kernel.Matrix([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [5615, 3944, 8021, 2117, -7732]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[1, -1, -5, 3, -3],[0, -1, 0, -12, 4],[0, -1, 7, 1, -11],[1, 1, 5, -4, 2],[0, 1, -3, -2, -14]]):
-		return False
+	matrices = [
+		[[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [5615, 3944, 8021, 2117, -7732]],
+		[[10, 11], [11, 12]],
+		[[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [15, 22, 13, -35]],
+		[[1, 0, 0], [0, 1, 0], [15, 22, -22]],
+		[[1, -1, 3], [1, 0, 5], [1, 2, 6]],
+		[[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [22, 87, 45, -67]],
+		[[1, -1], [-1, 2]],
+		[[2, 3], [0, 1]],
+		[[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [17, 8, 10, 14, 3, -30]],
+		[[263, -224, -3021], [289, -246, -3319], [850, -724, -9764]]
+		]
 	
-	M = flipper.kernel.Matrix([[10, 11], [11, 12]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[-1, 0],[0, 1]]):
-		return False
+	answers = [
+		[[1, -1, -5, 3, -3],[0, -1, 0, -12, 4],[0, -1, 7, 1, -11],[1, 1, 5, -4, 2],[0, 1, -3, -2, -14]],
+		[[-1, 0],[0, 1]],
+		[[0, -1, 2, -1],[1, 0, -2, -2],[1, 1, 1, 2],[0, -2, -1, 2]],
+		[[0, 3, -1],[1, 0, 0],[0, 1, 7]],
+		[[0, 1, -1],[1, 0, 0],[0, 1, 2]],
+		[[1, -2, 0, 1],[0, 0, 1, 8],[1, 1, 1, -1],[0, 1, -2, 3]],
+		[[1, 0],[0, -1]],
+		[[-1, 1],[1, 1]],
+		[[-1, -1, 0, 1, 0, -1],[1, 0, -1, 0, -2, 0],[1, 0, 1, 0, 0, -1],[0, 1, 0, 1, -1, 0],[0, 1, -1, 0, 0, -1],[1, 0, -1, 1, 0, 0]],
+		[[-1, 1, 0],[1, 1, 0],[0, 0, 2]]
+		]
 	
-	M = flipper.kernel.Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [15, 22, 13, -35]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[0, -1, 2, -1],[1, 0, -2, -2],[1, 1, 1, 2],[0, -2, -1, 2]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[1, 0, 0], [0, 1, 0], [15, 22, -22]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[0, 3, -1],[1, 0, 0],[0, 1, 7]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[1, -1, 3], [1, 0, 5], [1, 2, 6]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[0, 1, -1],[1, 0, 0],[0, 1, 2]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [22, 87, 45, -67]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[1, -2, 0, 1],[0, 0, 1, 8],[1, 1, 1, -1],[0, 1, -2, 3]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[1, -1], [-1, 2]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[1, 0],[0, -1]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[2, 3], [0, 1]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[-1, 1],[1, 1]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [17, 8, 10, 14, 3, -30]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[-1, -1, 0, 1, 0, -1],[1, 0, -1, 0, -2, 0],[1, 0, 1, 0, 0, -1],[0, 1, 0, 1, -1, 0],[0, 1, -1, 0, 0, -1],[1, 0, -1, 1, 0, 0]]):
-		return False
-	
-	M = flipper.kernel.Matrix([[263, -224, -3021], [289, -246, -3319], [850, -724, -9764]]).transpose()
-	N = M.LLL().transpose()
-	if verbose: print(N)
-	if N != flipper.kernel.Matrix([[-1, 1, 0],[1, 1, 0],[0, 0, 2]]):
-		return False
+	for matrix, answer in zip(matrices, answers):
+		M = flipper.kernel.Matrix(matrix).transpose()
+		N = M.LLL().transpose()
+		if verbose: print(N)
+		if N != flipper.kernel.Matrix(answer):
+			return False
 	
 	return True
 
 if __name__ == '__main__':
-	main(verbose=True)
+	print(main(verbose=True))
 

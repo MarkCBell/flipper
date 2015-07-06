ad8c3a7cb703d1f994e9f769d2966b21fddb7479
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2015-07-06 13:30:14 +0100

    Added additonal harder test.

--- source/profile/encoding.py
+++ source/profile/encoding.py
@@ -24,6 +24,7 @@ def main(verbose=False):
 		('S_1_2', 'bbaCBAaBabcABB', NT_TYPE_REDUCIBLE),
 		('S_1_2', 'aCBACBacbaccbAaAcAaBBcCcBBcCaBaaaABBabBcaBbCBCbaaa', NT_TYPE_PSEUDO_ANOSOV),
 		('S_2_1', 'aaabcd', NT_TYPE_PSEUDO_ANOSOV),
+		# ('S_5_1', 'abcdefgHIl', NT_TYPE_PSEUDO_ANOSOV),
 		('E_12', 'aaaaBBp', NT_TYPE_PSEUDO_ANOSOV),
 		('E_12', 'aaBaaBBp', NT_TYPE_REDUCIBLE),
 		('E_12', 'aaaaBBaBaBp', NT_TYPE_PSEUDO_ANOSOV),

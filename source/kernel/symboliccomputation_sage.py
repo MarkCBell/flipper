a96db3d7bfaddbb78ab5fe3708823f1607b227f8
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2015-07-06 13:18:12 +0100

    Better formatting.

--- source/kernel/symboliccomputation_sage.py
+++ source/kernel/symboliccomputation_sage.py
@@ -54,40 +54,40 @@ def directed_eigenvector(action_matrix, condition_matrix, vector):
 	Assumes that C contains at most one interesting eigenvector. '''
 	
 	M = Matrix(action_matrix.rows)
+	# Only bother checking the real, stable eigenspaces.
+	eigenvalues = [eigenvalue for eigenvalue in M.eigenvalues() if eigenvalue.imag() == 0 and eigenvalue > 1]
 	# We have to check ALL eigenspaces. So we do it in order of decreasing real part.
-	for eigenvalue in sorted(M.eigenvalues(), reverse=True, key=lambda z: complex(z).real):
-		# Only bother checking the real eigenspaces.
-		if eigenvalue.imag() == 0 and eigenvalue > 1:
-			[lam] = NumberField(eigenvalue.minpoly(), 'L', embedding=eigenvalue.n()).gens()
-			right_kernel = (M-lam).right_kernel().basis()
+	for eigenvalue in sorted(eigenvalues, reverse=True, key=lambda z: complex(z).real):
+		[lam] = NumberField(eigenvalue.minpoly(), 'L', embedding=eigenvalue.n()).gens()
+		right_kernel = (M-lam).right_kernel().basis()
+		
+		if len(right_kernel) == 1:  # If rank(kernel) == 1.
+			[eigenvector] = right_kernel
 			
-			if len(right_kernel) == 1:  # If rank(kernel) == 1.
-				[eigenvector] = right_kernel
-				
-				scale = abs(lcm([x.denominator() for entry in eigenvector for x in coefficients(entry.polynomial())]))
-				eigenvector_rescaled_coefficients = [[int(scale * x) for x in coefficients(entry.polynomial())] for entry in eigenvector]
-				
-				eigenvalue_coefficients = minpoly_coefficients(eigenvalue)
-				d = int(log(sum(abs(x) for x in eigenvalue_coefficients))) + 1
-				N = flipper.kernel.create_number_field(eigenvalue_coefficients, approximate(eigenvalue, 2*d + 1))
-				flipper_ev, flipper_eigenvector = N.lmbda, [N.element(entry) for entry in eigenvector_rescaled_coefficients]
-				
-				# We can't rely on Sage to check this lies in the cone as for elements of NumberFields:
-				#  x > y returns True
-				# See: https://groups.google.com/forum/#!topic/sage-devel/9eAZnOBvBHM
-				if flipper.kernel.matrix.nonnegative(flipper_eigenvector) and condition_matrix.nonnegative_image(flipper_eigenvector):
-					return flipper_ev, flipper_eigenvector
-			else:
-				eqns = [[0] + list(row) for row in (M - eigenvalue)]
-				ieqs = [[0] + list(row) for row in condition_matrix]
-				
-				# This is really slow.
-				P = Polyhedron(eqns=eqns, ieqs=ieqs)
-				# If dim(P) == 1 then an extremal ray of the cone is an eigenvector.
-				# As this is a rational vector it must correspond to a fixed curve.
-				# If dim(P)  > 1 then there is a high dimensional invariant subspace.
-				if eigenvalue > 1 and P.dim() > 0:
-					raise flipper.AssumptionError('Subspace is reducible.')
+			scale = abs(lcm([x.denominator() for entry in eigenvector for x in coefficients(entry.polynomial())]))
+			eigenvector_rescaled_coefficients = [[int(scale * x) for x in coefficients(entry.polynomial())] for entry in eigenvector]
+			
+			eigenvalue_coefficients = minpoly_coefficients(eigenvalue)
+			d = int(log(sum(abs(x) for x in eigenvalue_coefficients))) + 1
+			N = flipper.kernel.create_number_field(eigenvalue_coefficients, approximate(eigenvalue, 2*d + 1))
+			flipper_ev, flipper_eigenvector = N.lmbda, [N.element(entry) for entry in eigenvector_rescaled_coefficients]
+			
+			# We can't rely on Sage to check this lies in the cone as for elements of NumberFields:
+			#  x > y returns True
+			# See: https://groups.google.com/forum/#!topic/sage-devel/9eAZnOBvBHM
+			if flipper.kernel.matrix.nonnegative(flipper_eigenvector) and condition_matrix.nonnegative_image(flipper_eigenvector):
+				return flipper_ev, flipper_eigenvector
+		else:
+			eqns = [[0] + list(row) for row in (M - eigenvalue)]
+			ieqs = [[0] + list(row) for row in condition_matrix]
+			
+			# This is really slow.
+			P = Polyhedron(eqns=eqns, ieqs=ieqs)
+			# If dim(P) == 1 then an extremal ray of the cone is an eigenvector.
+			# As this is a rational vector it must correspond to a fixed curve.
+			# If dim(P)  > 1 then there is a high dimensional invariant subspace.
+			if eigenvalue > 1 and P.dim() > 0:
+				raise flipper.AssumptionError('Subspace is reducible.')
 	
 	raise flipper.ComputationError('No interesting eigenvalues in cell.')
 

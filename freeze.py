4a8a437dbf45a08daf8ec6d3468c2c5aa6554963
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2015-08-01 10:06:11 +0100

    Updated freeze to work with new directory structure.

--- freeze.py
+++ freeze.py
@@ -4,16 +4,19 @@ import os
 from cx_Freeze import setup, Executable
 
 # Get the correct version.
-flipper_directory = os.path.join(os.path.dirname(__file__), 'flipper_triangulation')
 from flipper.version import __version__
 
+flipper_directory = os.path.join(os.path.dirname(__file__), 'source')
 setup(
 	name='flipper',
-	version=__version__,  # Get the correct version.
+	version=__version__,  # Set the correct version.
 	author='Mark Bell',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
 	package_dir={'flipper': flipper_directory},
-	options={'build_exe': {'icon': './flipper_triangulation/app/icon/icon.ico', 'include_files': [('./flipper_triangulation/app/icon/', './icon')]}},
-	executables=[Executable('./flipper_triangulation/app/__main__.py', targetName='flipper', base=('Win32GUI' if sys.platform == 'win32' else None))]
+	options={'build_exe': {
+		'icon': os.path.join(flipper_directory, 'application', 'icon', 'icon.ico'),
+		'include_files': [(os.path.join(flipper_directory, 'application', 'icon'), './icon')]
+		}},
+	executables=[Executable(os.path.join(flipper_directory, 'app.py'), targetName='flipper', base=('Win32GUI' if sys.platform == 'win32' else None))]
 	)
 

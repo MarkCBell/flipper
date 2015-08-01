ff0fa6a6bc387af78e52a78d1e52b831e6efd5f5
Mark Bell (m c bell <Mark Bell (m.c.bell@warwick.ac.uk)>
2015-08-01 16:10:21 +0100

    Freeze now working on Windows.

--- freeze.py
+++ freeze.py
@@ -6,17 +6,24 @@ from cx_Freeze import setup, Executable
 # Get the correct version.
 from flipper.version import __version__
 
-flipper_directory = os.path.join(os.path.dirname(__file__), 'source')
+DIRECTORY = os.path.join(os.path.dirname(__file__), 'source')
+if sys.platform == 'win32':
+	BASE = 'Win32GUI'
+	TARGET_NAME = 'flipper.exe'
+else:
+	BASE = None
+	TARGET_NAME = 'flipper'
+
 setup(
 	name='flipper',
 	version=__version__,  # Set the correct version.
 	author='Mark Bell',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
-	package_dir={'flipper': flipper_directory},
+	package_dir={'flipper': DIRECTORY},
 	options={'build_exe': {
-		'icon': os.path.join(flipper_directory, 'application', 'icon', 'icon.ico'),
-		'include_files': [(os.path.join(flipper_directory, 'application', 'icon'), './icon')]
+		'icon': os.path.join(DIRECTORY, 'application', 'icon', 'icon.ico'),
+		'include_files': [(os.path.join(DIRECTORY, 'application', 'icon'), './icon')]
 		}},
-	executables=[Executable(os.path.join(flipper_directory, 'app.py'), targetName='flipper', base=('Win32GUI' if sys.platform == 'win32' else None))]
+	executables=[Executable(os.path.join(DIRECTORY, 'app.py'), targetName=TARGET_NAME, base=BASE)]
 	)
 

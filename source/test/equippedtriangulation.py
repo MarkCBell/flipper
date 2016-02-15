0ce787fa8f20fddccd62ea0774cd8b0f6678170a
Mark Bell (mcbell <Mark Bell (mcbell@illinois.edu)>
2016-02-15 14:31:59 -0600

    Fixed test for new EquippedTriangulation.all_words format.

--- source/test/equippedtriangulation.py
+++ source/test/equippedtriangulation.py
@@ -7,7 +7,7 @@ def main(verbose=False):
 	
 	S = flipper.load('S_1_2')
 	
-	all_words = set(S.all_words(5, group=False))
+	all_words = set(S.all_words(5, equivalence='None'))
 	# There should be 8^5 + 8^4 + ... + 8^0 = 37449 words.
 	if verbose: print('Constructed %d words.' % len(all_words))
 	for _ in range(10):

2916632a6534f8b5c197e11ce573df14edb3193d
Mark Bell (mcbell <Mark Bell (mcbell@illinois.edu)>
2016-04-08 14:57:08 -0500

    Starting the app now uses ArgParse.

--- source/app.py
+++ source/app.py
@@ -1,9 +1,14 @@
 
 import sys
+import argparse
 import flipper.application
 
 start = flipper.application.start
 
 if __name__ == '__main__':
-	start(load_from=(None if len(sys.argv) == 1 else sys.argv[1]))
+	parser = argparse.ArgumentParser(description='Flipper GUI')
+	parser.add_argument('load', nargs='?', help='path to load from when starting')
+	args = parser.parse_args()
+	
+	start(load_from=args.load)
 

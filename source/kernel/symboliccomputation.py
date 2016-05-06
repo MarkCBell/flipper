25c51d19ea8abf5400c64d739cdfb47e3d7025ba
Mark Bell (mcbell <Mark Bell (mcbell@illinois.edu)>
2016-05-06 19:47:19 +0100

    Now refer to these libraries as interfaces.

--- source/kernel/symboliccomputation.py
+++ source/kernel/symboliccomputation.py
@@ -1,8 +1,8 @@
 
 ''' A module for interacting with various symbolic calculation libraries.
 
-This module selects and imports the appropriate library for manipulating algebraic numbers.
-Currently there is an interface to sage and a dummy library. A library should raise an
+This module selects and imports the appropriate interface for manipulating algebraic numbers.
+Currently there is an interface to sage, cypari and a dummy library. A library should raise an
 ImportError on import if it cannot be used. The dummy library can always be used, even in
 Python 3.
 
@@ -20,30 +20,32 @@ More interfaces can be added here. '''
 from importlib import import_module
 
 # Add new libraries here in order.
-LIBRARIES = ['sage', 'cypari', 'dummy']
+INTERFACES = ['sage', 'cypari', 'dummy']
 
-def load_library():
-	''' Return the first available library.
+def load_interface():
+	''' Return the first available interface.
 	
-	If none are available then an ImportError will be raised. '''
+	If none are available then an ImportError will be raised.
+	This should nver happen thanks to the dummy interface provided. '''
 	
-	for library in LIBRARIES:
+	for interface in INTERFACES:
 		try:
-			module = import_module('flipper.kernel.interface.' + library)
-			print(library)
+			module = import_module('flipper.kernel.interface.' + interface)
+			# We could add some code here to find out which interface was loaded.
 			return module
 		except ImportError:
 			pass
 	
-	raise ImportError('No symbolic computation library available.')
+	raise ImportError('No symbolic computation interface available.')
 
 def directed_eigenvector(action_matrix, condition_matrix):
 	''' Return an interesting eigenvector of action_matrix which lives inside of the cone C, defined by condition_matrix.
 	
-	An eigenvector is interesting if its corresponding eigenvalue is: real, > 1 and irrational.
+	An eigenvector is interesting if its corresponding eigenvalue is: real, > 1, irrational and bigger than all
+	of its Galois conjugates.
 	
 	Raises a ComputationError if it cannot find an interesting vectors in C.
 	Assumes that C contains at most one interesting eigenvector. '''
 	
-	return load_library().directed_eigenvector(action_matrix, condition_matrix)
+	return load_interface().directed_eigenvector(action_matrix, condition_matrix)
 

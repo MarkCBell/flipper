4f1891c7bb84f6efc9e7f7a0099f9d52e42cd1f8
Mark Bell <mcbell@illinois.edu>
2016-05-30 10:38:16 +0100

    Updated email address.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 	version=__version__,
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
 	author='Mark Bell',
-	author_email='M.C.Bell@warwick.ac.uk',
+	author_email='mcbell@illinois.edu',
 	url='https://bitbucket.org/Mark_Bell/flipper',
 	# Remember to update these if the directory structure changes.
 	packages=[

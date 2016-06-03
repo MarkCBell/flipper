d8cf076ab5b3afad884004324071803b90d82d1b
Mark Bell <mcbell@illinois.edu>
2016-06-03 22:46:38 +0100

    Now just have integer type.

--- source/kernel/types.py
+++ source/kernel/types.py
@@ -1,15 +1,9 @@
 
 ''' A module for the standard types.
 
-Provides a standard reference for the type of integers, strings, etc. as these depend on the
-version of Python being used. This is similar to the six module. '''
+Provides a standard reference for the type of integers. This is similar to the six module. '''
 
-import sys
 import numbers
 
-if sys.version_info >= (3, 0):
-	IntegerType = numbers.Integral
-else:
-	# In Python 2.x an integer can be an int or long (Python automatically switches to longs when required).
-	IntegerType = numbers.Integral
+IntegerType = numbers.Integral
 

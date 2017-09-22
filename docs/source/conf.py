98969d59ff7d4a1c85778da52477339be8688c62
Mark Bell <mcbell@illinois.edu>
2017-09-22 15:56:07 +0100

    Added favicon and logo.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,8 @@ html_theme = 'alabaster'
 # documentation.
 #
 # html_theme_options = {}
+html_favicon = '../../flipper/application/icon/icon.ico'
+html_logo = '../../flipper/application/icon/icon.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

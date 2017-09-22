8d4e185b46c9279c290d2ce2d744eb633d5d9a84
Mark Bell <mcbell@illinois.edu>
2017-09-22 16:50:00 +0100

    Updated freeze.

--- freeze.py
+++ freeze.py
@@ -4,9 +4,9 @@ import os
 from cx_Freeze import setup, Executable
 
 # Get the correct version.
-from flipper.version import __version__
+from flipper import __version__
 
-DIRECTORY = os.path.join(os.path.dirname(__file__), 'source')
+DIRECTORY = os.path.join(os.path.dirname(__file__), 'flipper')
 if sys.platform == 'win32':
 	BASE = 'Win32GUI'
 	TARGET_NAME = 'flipper.exe'
@@ -19,11 +19,9 @@ setup(
 	version=__version__,  # Set the correct version.
 	author='Mark Bell',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
-	package_dir={'flipper': DIRECTORY},
 	options={'build_exe': {
-		'icon': os.path.join(DIRECTORY, 'application', 'icon', 'icon.ico'),
-		'include_files': [(os.path.join(DIRECTORY, 'application', 'icon'), './icon')]
+		'include_files': [('flipper/application/icon', './icon')]
 		}},
-	executables=[Executable(os.path.join(DIRECTORY, 'app.py'), targetName=TARGET_NAME, base=BASE)]
+	executables=[Executable(os.path.join(DIRECTORY, 'app.py'), targetName=TARGET_NAME, base=BASE, icon='flipper/application/icon/icon.ico')]
 	)
 

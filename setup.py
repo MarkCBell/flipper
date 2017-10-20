6188792a00cca1ced92bf66aa48f6d6e61b661d2
Mark Bell <mcbell@illinois.edu>
2017-10-21 00:24:16 +0100

    Added long_description.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 
+import os
 try:
 	from setuptools import setup
 except ImportError:
 	print('Unable to import setuptools, using distutils instead.')
 	from distutils.core import setup
 
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
 	name='flipper',
 	version='0.12.2',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
+	long_description=read('README'),
 	author='Mark Bell',
 	author_email='mcbell@illinois.edu',
 	url='https://bitbucket.org/Mark_Bell/flipper',

d4758b5858ada3f71f39250718d5427ceed71407
Mark Bell <mark00bell@googlemail.com>
2018-06-24 11:19:26 +0100

    Standardised.

--- setup.py
+++ setup.py
@@ -1,47 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import os
-try:
-	from setuptools import setup
-except ImportError:
-	print('Unable to import setuptools, using distutils instead.')
-	from distutils.core import setup
+'''The setup script.'''
 
-def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools import setup, find_packages
+
+requirements = [
+    'panda',
+    'numpy',
+    'cypari'
+]
 
 setup(
 	name='flipper',
 	version='0.12.3',
 	description='For manipulating curves and measured laminations on surfaces and producing mapping tori.',
-	long_description=read('README'),
-	author='Mark Bell',
-	author_email='mcbell@illinois.edu',
-	url='https://bitbucket.org/Mark_Bell/flipper',
-	# Remember to update these if the directory structure changes.
-	packages=[
-		'flipper',
-		'flipper.application',
-		'flipper.kernel',
-		'flipper.kernel.interface',
-		'flipper.test',
-		'flipper.profile'
-		],
-	package_data={
-		'flipper': ['censuses/*.csv'],
-		'flipper.application': ['icon/*', 'frames/*'],
-		},
-	install_requires=[
-		'pandas',  # censuses.
-		],
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Education',
-		'Intended Audience :: Science/Research',
-		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Topic :: Scientific/Engineering :: Mathematics',
-		],
-	)
+    long_description='See http://flipper.readthedocs.io for the full README, LICENCE and documentation.',
+    author='Mark Bell',
+    author_email='mcbell@illinois.edu',
+    url='https://github.com/MarkCBell/flipper',
+    packages=find_packages(),
+    package_data={
+        'flipper.application': ['icon/*'],
+        },
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT License',
+    zip_safe=False,
+    keywords='curver',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+)
 

fe033a870816ba1253890cfef0631bf97ba02bf1
Mark Bell <mark00bell@googlemail.com>
2018-07-01 15:55:52 +0100

    Fixed rewriting typo.

--- docs/user/samples/distribution.py
+++ docs/user/samples/distribution.py
@@ -5,6 +5,6 @@ num_samples = 100
 
 S = flipper.load('S_2_1')
 for i in range(length):
-    pA_samples = sum(1 if S.mapping_class(i).is_pseudo_anosov() else 0 if j for j in range(num_samples))
+    pA_samples = sum(1 if S.mapping_class(i).is_pseudo_anosov() else 0 for _ in range(num_samples))
     print('Length %d: %0.1f%% pA' % (i, float(pA_samples) * 100 / num_samples))
 
